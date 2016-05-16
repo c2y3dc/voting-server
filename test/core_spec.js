1a42a2e9eff0949505d1f33cd0f33138a13c7575
Ervin Chow <ervinchow@gmail.com>
2016-05-16 13:53:34 -0700

    adds vote function and test cases

--- test/core_spec.js
+++ test/core_spec.js
@@ -1,7 +1,7 @@
 import { List, Map } from 'immutable'
 import { expect } from 'chai'
 
-import { setEntries, next } from '../src/core'
+import { setEntries, next, vote } from '../src/core'
 
 describe('application logic', () => {
 
@@ -36,6 +36,51 @@ describe('application logic', () => {
 					pair: List.of('Trainspotting', '28 Days Later')
 				}),
 				entries: List.of('Sunshine')
+			}))
+		})
+	})
+
+	describe('vote', () => {
+		it('creates a tally for voted entry', () => {
+			const state = Map({
+				vote: Map({
+					pair: List.of('Trainspotting', '28 Days Later')
+				}),
+				entries: List()
+			})
+			const nextState = vote(state, 'Trainspotting')
+			expect(nextState).to.equal(Map({
+				vote: Map({
+					pair: List.of('Trainspotting', '28 Days Later'),
+					tally: Map({
+						'Trainspotting': 1
+					})
+				}),
+				entries: List()
+			}))
+		})
+
+		it('adds to existing tally for the voted entry', () => {
+			const state = Map({
+				vote: Map({
+					pair: List.of('Trainspotting', '28 Days Later'),
+					tally: Map({
+						'Trainspotting': 3,
+						'28 Days Later': 2
+					})
+				}),
+				entries: List()
+			})
+			const nextState = vote(state, 'Trainspotting')
+			expect(nextState).to.equal(Map({
+				vote: Map({
+					pair: List.of('Trainspotting', '28 Days Later'),
+					tally: Map({
+						'Trainspotting': 4,
+						'28 Days Later': 2
+					})
+				}),
+				entries: List()
 			}))
 		})
 	})
