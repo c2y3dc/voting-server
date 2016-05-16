5bf7a7edb3a1065c2c3507baa3f0a58d11684d53
Ervin Chow <ervinchow@gmail.com>
2016-05-15 22:50:23 -0700

    adds start vote test case to core

--- test/core_spec.js
+++ test/core_spec.js
@@ -23,4 +23,20 @@ describe('application logic', () => {
 			}))
 		})
 	})
+
+	describe('next', () => {
+		it('takes the next two entries under vote', () => {
+			const state = Map({
+				entries: List.of('Trainspotting', '28 Days Later', 'Sunshine')
+			})
+			const nextState = next(state)
+
+			expect(nextState).to.equal(Map({
+				vote: Map({
+					pair: List.of('Trainspotting', '28 Days Later')
+				}),
+				entries: List.of('Sunshine')
+			}))
+		})
+	})
 })
